8a5ddeffcf1c6bbd846a35301293ed8ecf104d79
Matheus Regis <matheustrai@gmail.com>
2025-01-25 18:59:24 -0300

    feat - Added Pricing section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Header from "./components/landing-page/Header";
 import Hero from "./components/landing-page/Hero";
+import Pricing from "./components/landing-page/Pricing";
 import VideoExplanation from "./components/landing-page/VideoExplanation";
 
 export default function Home() {
@@ -8,8 +9,8 @@ export default function Home() {
       <Header />
       <Hero />
       <VideoExplanation />
-      {/* 
       <Pricing />
+      {/* 
       <FAQ /> */}
     </div>
   );
