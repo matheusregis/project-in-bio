cec96f4b23dd86f6fd479945f3e8a2094175f916
Matheus Regis <matheustrai@gmail.com>
2025-01-25 18:46:57 -0300

    feat - Created Video explanation to LP

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import Header from "./components/landing-page/Header";
 import Hero from "./components/landing-page/Hero";
+import VideoExplanation from "./components/landing-page/VideoExplanation";
 
 export default function Home() {
   return (
     <div className="max-w-7xl mx-auto">
       <Header />
       <Hero />
-      {/* 
       <VideoExplanation />
+      {/* 
       <Pricing />
       <FAQ /> */}
     </div>
